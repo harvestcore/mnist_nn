ebf3ec68c33ad6fb2538a7ffce6f887f9fbbb193
harvestcore <aagomezies@gmail.com>
2020-12-06 16:44:34 +0100

    Finish NN + cleanup

--- src/neural_network.py
+++ src/neural_network.py
@@ -1,6 +1,9 @@
-from math import sqrt
-from random import randrange
+import numpy as np
+import time
 
+from tensorflow.keras.datasets import mnist
+
+from utils import load_datasets
 import activation_functions as af
 from activation_functions import ACTIVATION_FUNCTIONS
 
@@ -9,8 +12,6 @@ class NeuralNetwork:
         self.layers = layers
         self.noof_layers = len(layers) - 1
         self.input_layer = layers[0]
-        self.output_layer = layers[len(layers) - 1]
-        self.hidden_layers = layers[1:len(layers) - 1]
         self.epochs = epochs
         self.rate = rate
         self.activation = getattr(af, activation) if activation in ACTIVATION_FUNCTIONS else getattr(af, 'sigmoid')
@@ -19,22 +20,85 @@ class NeuralNetwork:
     def generate_weights(self):
         weights = {}
 
+        current_layer = self.input_layer
         for i in range(self.noof_layers):
             index = i + 1
-            key = 'w' + str(index)
-            a = self.layers[index]
-            b = self.input_layer
-
-            if a > b:
-                a = self.input_layer
-                b = self.layers[index]
-                
-            weights[key] = randrange(a, b) * sqrt(1.0 / self.layers[index])
+            key = 'W' + str(index)
+            weights[key] = np.random.randn(self.layers[index], current_layer) * np.sqrt(1. / self.layers[index])
+            current_layer = self.layers[index]
 
         return weights
 
-    def forward_propagation(self):
-        return 1
+    def back_propagation(self, values, output):
+        weights = self.weights
+        changes = {}
 
-    def back_propagation(self):
-        return 1
+        # Last layer
+        z_key = 'Z' + str(self.noof_layers)
+        w_key = 'W' + str(self.noof_layers)
+        a_key = 'A' + str(self.noof_layers - 1)
+
+        # Latest layer error
+        error = 2 * (output - values) / output.shape[0] * self.activation(weights[z_key])
+        changes[w_key] = np.outer(error, weights[a_key])
+
+        # Previous layers
+        for i in range(self.noof_layers, 1, -1):
+            w_key = 'W' + str(i)
+            w_previous_key = 'W' + str(i - 1)
+            z_key = 'Z' + str(i - 1)
+            a_key = 'A' + str(i - 2)
+
+            if i - 2 > 0:
+                error = np.dot(weights[w_key].T, error) * self.activation(weights[z_key])
+                changes[w_previous_key] = np.outer(error, weights[a_key])
+
+        self.update(changes)
+
+    def forward_propagation(self, values):
+        weights = self.weights
+
+        # First layer values
+        weights['A0'] = values
+
+        for i in range(1, self.noof_layers + 1, 1):
+            keyLastA = 'A' + str(i - 1)
+            keyW = 'W' + str(i)
+            keyA = 'A' + str(i)
+            keyZ = 'Z' + str(i)
+
+            weights[keyZ] = np.dot(weights[keyW], weights[keyLastA])
+            weights[keyA] = self.activation(weights[keyZ])
+
+        return weights['A' + str(self.noof_layers)]
+
+    def update(self, changes):       
+        for key, value in changes.items():
+            self.weights[key] -= self.rate * value
+
+    def accuracy(self, x, y):
+        acc = []
+
+        for _x, _y in zip(x, y):
+            output = self.forward_propagation(_x)
+            pred = np.argmax(output)
+            acc.append(pred == np.argmax(_y))
+        
+        return np.mean(acc) * 100
+
+
+    def train(self, x_train, y_train, x, y):
+        results = []
+        start_time = time.time()
+        for epoch in range(1, self.epochs + 1, 1):
+            for _x, _y in zip(x_train, y_train):
+                output = self.forward_propagation(_x)
+                self.back_propagation(_y, output)
+       
+            results.append([
+                epoch,                      # Epoch
+                self.accuracy(x, y),        # Accuracy
+                time.time() - start_time    # Time spent
+            ])
+            
+        return results
