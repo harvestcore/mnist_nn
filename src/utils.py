d929e7f7c4ffa93cd7024d1d762161faaec1a56e
harvestcore <aagomezies@gmail.com>
2020-12-06 16:39:29 +0100

    Export to CSV + getfilename

--- src/utils.py
+++ src/utils.py
@@ -1,12 +1,7 @@
 from mnist import MNIST
 import numpy as np
-
-# Load the datasets.
-# Makes use of the MNIST library (https://pypi.org/project/python-mnist/).
-def load_datasets():
-    data = MNIST('./datasets')
-    images, labels = data.load_training()
-    return images, labels
+from datetime import datetime
+import csv
 
 # Normalize a dataset.
 # Makes use of the normalization function from numpy library.
@@ -17,3 +12,24 @@ def normalize(dataset):
 
     return normalized
 
+# Load the datasets.
+# Makes use of the MNIST library (https://pypi.org/project/python-mnist/).
+def load_datasets():
+    data = MNIST('./datasets')
+    images, labels = data.load_training()
+    return normalize(images), labels
+
+def to_csv(filename, data):
+    with open(filename, mode='a') as file:
+        writer = csv.writer(file, delimiter=',')
+        writer.writerows(data)
+
+def get_filename(epochs, rate, layers, activation):
+    filename = 'epochs-' + str(epochs)
+    filename += '_rate-' + str(rate)
+    filename += '_activation-' + activation
+    filename += '_layers-' + ','.join(map(str, layers))
+    filename += '_' + str(datetime.now())
+    filename += '.csv'
+
+    return filename
