Some fixes + add sigmoid derivative

src/activation_functions.py
@@ -1,9 +1,13 @@
-from math import sqrt, exp, tanh, pi
+import numpy as np
+from math import sqrt, tanh, pi
 
-ACTIVATION_FUNCTIONS = ['sigmoid', 'relu', 'elu', 'lrelu', 'softmax', 'gelu']
+ACTIVATION_FUNCTIONS = ['sigmoid', 'sigmoid_derivative', 'relu', 'elu', 'lrelu', 'softmax', 'gelu']
 
 def sigmoid(value):
-        return 1 / (1 + exp(-value))
+    return 1 / (1 + np.exp(-value))
+
+def sigmoid_derivative(value):
+    return (np.exp(-value))/((np.exp(-value)+1)**2)
 
 def relu(value):
     return max(0, value)
@@ -12,7 +16,7 @@ def elu(value, alpha=0.2):
     if value > 0:
         return value
     
-    return alpha * (exp(value) - 1)
+    return alpha * (np.exp(value) - 1)
 
 def lrelu(value, alpha=0.2):
     if value > 0:
@@ -21,7 +25,7 @@ def lrelu(value, alpha=0.2):
     return alpha * value
 
 def softmax(values):
-    xp = exp(values - max(values))
+    xp = np.exp(values - max(values))
     return xp / sum(xp)
 
 def gelu(value):